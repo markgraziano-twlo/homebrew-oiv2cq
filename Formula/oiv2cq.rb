c08a82a0f7875d3fdce1c135b044a072458899a0
mgraziano-twlo <mgraziano@twilio.com>
2025-01-22 14:14:29 -0600

    Fix formula to correctly install oiv2cq package

--- Formula/oiv2cq.rb
+++ Formula/oiv2cq.rb
@@ -1,23 +1,20 @@
 class Oiv2cq < Formula
   desc "CLI to automate onboarding and plugin setup for Twilio's CloudQuery projects"
-  homepage "https://github.com/markgraziano-twlo/oiv2cq"
-  url "https://github.com/markgraziano-twlo/oiv2cq/releases/download/v1.0.0/oiv2cq-v1.0.0.tar.gz"
+  homepage "https://github.com/markgraziano-twlo/homebrew-oiv2cq"
+  url "https://github.com/markgraziano-twlo/homebrew-oiv2cq/releases/download/v1.0.0/oiv2cq-v1.0.0.tar.gz"
   sha256 "389342d3351b621c2e529c9449c0f354f63734a8fd2fb57d05e10a676bf33619"
   license "MIT"
 
   depends_on "python@3.9"
 
   def install
-    # Install all Python files
-    libexec.install Dir["*"]
+    # Install the oiv2cq package into libexec
+    libexec.install "oiv2cq", "requirements.txt", "setup.py"
 
-    # Dynamically find the Python binary
-    python_path = `which python3.9`.strip
-
-    # Create a wrapper script that uses the dynamically located Python
+    # Create a wrapper script to invoke cli.py
     (bin/"oiv2cq").write <<~EOS
       #!/bin/bash
-      #{python_path} #{libexec}/cli.py "$@"
+      #{Formula["python@3.9"].opt_bin}/python3 #{libexec}/oiv2cq/cli.py "$@"
     EOS
     chmod "+x", bin/"oiv2cq"
   end
