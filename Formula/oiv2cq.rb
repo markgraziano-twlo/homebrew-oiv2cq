1a5aefd72a54e0b5c5e11a3e6bb91995a0915cc9
mgraziano-twlo <mgraziano@twilio.com>
2025-01-22 15:13:13 -0600

    Update formula to fix dependency installation

--- Formula/oiv2cq.rb
+++ Formula/oiv2cq.rb
@@ -2,16 +2,16 @@ class Oiv2cq < Formula
   desc "CLI to automate onboarding and plugin setup for Twilio's CloudQuery projects"
   homepage "https://github.com/markgraziano-twlo/homebrew-oiv2cq"
   url "https://github.com/markgraziano-twlo/homebrew-oiv2cq/releases/download/v1.0.0/oiv2cq-v1.0.0.tar.gz"
-  sha256 "50274dc1dc2571b4f3a13be6c581b92732f90ea533a135e4a94dc08ade12e7f8"
+  sha256 "50274dc1dc2571b4f3a13be6c581b92732f90ea533a135e4a94dc08ade12e7f8" # Ensure the existing tarball's checksum is used
   license "MIT"
 
   depends_on "python"
 
   def install
-    # Install the oiv2cq directory and supporting files
+    # Install the CLI files
     libexec.install "oiv2cq", "requirements.txt", "setup.py"
 
-    # Install Python dependencies into libexec
+    # Install Python dependencies in the libexec environment
     system Formula["python"].opt_bin/"pip3", "install", "-r", libexec/"requirements.txt", "--target", libexec
 
     # Create a wrapper script
