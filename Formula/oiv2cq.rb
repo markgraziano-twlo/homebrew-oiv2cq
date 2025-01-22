609b9b6908ed8ee44fb2fbdba367729123cab4cb
mgraziano-twlo <mgraziano@twilio.com>
2025-01-22 13:38:06 -0600

    added brew tap prereqs and updated sha256 string

--- Formula/oiv2cq.rb
+++ Formula/oiv2cq.rb
@@ -2,7 +2,7 @@ class Oiv2cq < Formula
   desc "CLI to automate onboarding and plugin setup for Twilio's CloudQuery projects"
   homepage "https://github.com/markgraziano-twlo/oiv2cq"
   url "https://github.com/markgraziano-twlo/oiv2cq/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "e4ab9465f554c1a24698ee87677112efbd9c5c61832736428b962d6e86f13384"
+  sha256 "389342d3351b621c2e529c9449c0f354f63734a8fd2fb57d05e10a676bf33619"
   license "MIT"
 
   depends_on "python@3.9"
