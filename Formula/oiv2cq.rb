9ce463e382050a37b6cad316cb96147c4da905f8
mgraziano-twlo <mgraziano@twilio.com>
2025-01-22 13:46:53 -0600

    Add Homebrew formula for oiv2cq

--- Formula/oiv2cq.rb
+++ Formula/oiv2cq.rb
@@ -1,7 +1,7 @@
 class Oiv2cq < Formula
   desc "CLI to automate onboarding and plugin setup for Twilio's CloudQuery projects"
   homepage "https://github.com/markgraziano-twlo/oiv2cq"
-  url "https://github.com/markgraziano-twlo/oiv2cq/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/markgraziano-twlo/oiv2cq/releases/download/v1.0.0/oiv2cq-v1.0.0.tar.gz"
   sha256 "389342d3351b621c2e529c9449c0f354f63734a8fd2fb57d05e10a676bf33619"
   license "MIT"
 
