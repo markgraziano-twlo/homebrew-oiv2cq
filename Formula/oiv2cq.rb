7b963bfbb24da1ea8394d71b3280b650e5da473a
mgraziano-twlo <mgraziano@twilio.com>
2025-01-22 15:00:35 -0600

    Update formula to fix dependency installation

--- Formula/oiv2cq.rb
+++ Formula/oiv2cq.rb
@@ -1,23 +1,24 @@
 class Oiv2cq < Formula
   desc "CLI to automate onboarding and plugin setup for Twilio's CloudQuery projects"
   homepage "https://github.com/markgraziano-twlo/homebrew-oiv2cq"
-  url "https://github.com/markgraziano-twlo/homebrew-oiv2cq/releases/download/v1.0.2/oiv2cq-v1.0.2.tar.gz"
-  sha256 "9350c8cc242e67fc58c539e0e8b7a29bf10835333d25382e59d5fbe086cfb32e "
+  url "https://github.com/markgraziano-twlo/homebrew-oiv2cq/releases/download/v1.0.0/oiv2cq-v1.0.0.tar.gz"
+  sha256 "eb86caf46aec48747da028465d61f79a20ededbb39d19831f41b3bf36f15ab14"
   license "MIT"
 
-  depends_on "python"  # Use the latest Python version
+  depends_on "python"
 
   def install
+    # Install the oiv2cq directory and supporting files
     libexec.install "oiv2cq", "requirements.txt", "setup.py"
 
-    # Dynamically resolve Python path
-    python_path = "#{Formula["python"].opt_bin}/python3"
+    # Install Python dependencies into libexec
+    system Formula["python"].opt_bin/"pip3", "install", "-r", libexec/"requirements.txt", "--target", libexec
 
-    # Create a wrapper script for the CLI
+    # Create a wrapper script
     (bin/"oiv2cq").write <<~EOS
-        #!/bin/bash
-        export PYTHONPATH=#{libexec}
-        #{Formula["python"].opt_bin}/python3 #{libexec}/oiv2cq/cli.py "$@"
+      #!/bin/bash
+      export PYTHONPATH=#{libexec}
+      #{Formula["python"].opt_bin}/python3 #{libexec}/oiv2cq/cli.py "$@"
     EOS
     chmod "+x", bin/"oiv2cq"
   end
