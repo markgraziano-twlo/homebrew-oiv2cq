b3f1470f7ecd72e32696c6c637d4a1d3d903fbbc
markgraziano-twlo <mgraziano@twilio.com>
2025-01-22 14:54:29 -0600

    Update oiv2cq.rb
    delete space in sha256

--- Formula/oiv2cq.rb
+++ Formula/oiv2cq.rb
@@ -2,7 +2,7 @@ class Oiv2cq < Formula
   desc "CLI to automate onboarding and plugin setup for Twilio's CloudQuery projects"
   homepage "https://github.com/markgraziano-twlo/homebrew-oiv2cq"
   url "https://github.com/markgraziano-twlo/homebrew-oiv2cq/releases/download/v1.0.2/oiv2cq-v1.0.2.tar.gz"
-  sha256 "9350c8cc242e67fc58c539e0e8b7a29bf10835333d25382e59d5fbe086cfb32e "
+  sha256 "9350c8cc242e67fc58c539e0e8b7a29bf10835333d25382e59d5fbe086cfb32e"
   license "MIT"
 
   depends_on "python"  # Use the latest Python version
