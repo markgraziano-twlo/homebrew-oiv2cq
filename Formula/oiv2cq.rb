2541009e05ecd0ac8ac48bb7ebfeb1573e2b455c
mgraziano-twlo <mgraziano@twilio.com>
2025-01-30 15:30:02 -0600

    Fix formula installation path

--- Formula/oiv2cq.rb
+++ Formula/oiv2cq.rb
@@ -9,20 +9,20 @@ class Oiv2cq < Formula
 
   def install
     # Install CLI files
-    libexec.install "oiv2cq", "requirements.txt", "setup.py", "prereqs.py", "template_create.py", "cli.py"
+    libexec.install Dir["*"]
 
     # Create and set up the virtual environment inside libexec
     system Formula["python"].opt_bin/"python3", "-m", "venv", "#{libexec}/venv"
     system "#{libexec}/venv/bin/pip", "install", "-r", "#{libexec}/requirements.txt"
 
-    # Create a wrapper script to activate the virtual environment and run the CLI
-    (bin/"oiv2cq").write <<~EOS
-      #!/bin/bash
-      source "#{libexec}/venv/bin/activate"
-      python3 #{libexec}/cli.py "$@"
-    EOS
-    chmod "+x", bin/"oiv2cq"
-  end
+     # Create a wrapper script
+  (bin/"oiv2cq").write <<~EOS
+    #!/bin/bash
+    export PYTHONPATH=#{libexec}
+    #{Formula["python"].opt_bin}/python3 #{libexec}/oiv2cq/cli.py "$@"
+  EOS
+  chmod "+x", bin/"oiv2cq"
+end
 
   test do
     system "#{bin}/oiv2cq", "--help"
